0e12cfbe61d9f9d2494a22b176a6d680c9f2c536
Rodrigo Ramirez <rodrigoramirez0202@gmail.com>
2019-07-01 22:27:33 +0200

    Creación de menú I. Se ha creado un menu con dos items (informacion y configuracion) vídeo 33

--- app/src/main/java/rodrigoramirez0202/com/rocas/MainActivity.java
+++ app/src/main/java/rodrigoramirez0202/com/rocas/MainActivity.java
@@ -3,6 +3,8 @@ package rodrigoramirez0202.com.rocas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +13,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
     public void informacion(View vista){
 
         Intent intent=new Intent(this, InfoClase.class);
         startActivity(intent);
     }
+
+    public void salirApp (View vista){
+
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu mimenu){
+
+        getMenuInflater().inflate(R.menu.menu_en_activity, mimenu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem miitem){
+
+        int id=miitem.getItemId();
+
+        if(id==R.id.info){
+
+            informacion(null);
+            return true;
+        }else if(id==R.id.configuracion){
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(miitem);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
