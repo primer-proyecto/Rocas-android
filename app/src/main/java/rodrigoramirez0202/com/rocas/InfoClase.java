2264506fb311ee99ac47826370af48bd45d4a05d
Rodrigo Ramirez <rodrigoramirez0202@gmail.com>
2019-07-01 16:37:44 +0200

    Creación de Activity I. Se ha creado un Intent Vídeo 30.

---
+++ app/src/main/java/rodrigoramirez0202/com/rocas/InfoClase.java
@@ -0,0 +1,12 @@
+package rodrigoramirez0202.com.rocas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoClase extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
