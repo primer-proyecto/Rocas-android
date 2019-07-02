bbbe8d5c7415fa1afa2c856ca42c83b0daf8f616
Rodrigo Ramirez <rodrigoramirez0202@gmail.com>
2019-07-02 12:31:23 +0200

    Creación de menú III. Creacion de toolbar Vídeo 35

--- app/src/main/java/rodrigoramirez0202/com/rocas/MainActivity.java
+++ app/src/main/java/rodrigoramirez0202/com/rocas/MainActivity.java
@@ -3,17 +3,24 @@ package rodrigoramirez0202.com.rocas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar=findViewById(R.id.tool_bar);
+
+        setSupportActionBar(toolbar);
     }
 
     public void informacion(View vista){
